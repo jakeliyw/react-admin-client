c6c63070acefb8164eda0f5ab57c44a37ff85793
jakeliyw <1319569288@qq.com>
2021-02-20 16:32:30 +0800

    '项目实现登录静态页面待封装'

---
+++ src/index.js
@@ -0,0 +1,10 @@
+/**
+入口js
+*/
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+// import 'antd/dist/antd.css'
+
+// 将App组件标签渲染到index页面的div上
+ReactDOM.render(<App/>, document.getElementById('root'))
