857421caa9fe34179578eaafc754dc368b5a4a00
jakeliyw <1319569288@qq.com>
2021-02-24 23:08:10 +0800

    '头部静态完成'

--- src/component/header/index.jsx
+++ src/component/header/index.jsx
@@ -1,13 +1,26 @@
-import React, { Component }  from "react"
-import './index.less'
+import React, { Component } from "react";
+import "./index.less";
 /*
-* 头部组件
-*
-* */
+ * 头部组件
+ *
+ * */
 export default class Header extends Component {
   render() {
     return (
-      <div className="header">Header</div>
-    )
+      <div className="header">
+        <div className="header-top">
+          <span>欢迎，admin</span>
+          <a href="#javascript">退出</a>
+        </div>
+        <div className="header-bottom">
+          <div className="header-bottom-left">首页</div>
+          <div className="header-bottom-right">
+            <span>2020-5-26</span>
+            <img src="https://restapi.amap.com/v3/ip?ip=192.168.1.222&output=json&key=ffdccc2f20f7b15efe32d6cb2d805bc7" alt="weather"/>
+            <span>晴</span>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
